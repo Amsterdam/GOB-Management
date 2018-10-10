67f5b5bc244e8412b0c0516cfccfff01e562c557
Jasper Jager <mail@jasperjager.com>
2018-10-10 18:39:35 +0200

    Expand initial version
    Added README, requirements and docker files. Added the correct model for logs. Rearranged some files for clearer structure and added the possibility to filter logs on process_id.

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,16 +1,40 @@
 import graphene
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 
-from api.models import Log
+from graphene_sqlalchemy import SQLAlchemyObjectType
 
-# Schema Objects
-class LogObject(SQLAlchemyObjectType):
+from api.database.models import Log
+from api.fields import FilterConnectionField
+
+# Create a generic class to mutualize description of people attributes for both queries and mutations
+class LogAttribute:
+    logid = graphene.Int(description="Name of the person.")
+    timestamp = graphene.DateTime(description="Height of the person.")
+    process_id = graphene.String(description="Mass of the person.")
+    source = graphene.String(description="Hair color of the person.")
+    entity = graphene.String(description="Skin color of the person.")
+    level = graphene.String(description="Eye color of the person.")
+    name = graphene.String(description="Birth year of the person.")
+    msg = graphene.String(description="Gender of the person.")
+    data = graphene.JSONString(description="Global Id of the planet from which the person comes from.")
+
+
+class LogType(SQLAlchemyObjectType):
+    """Log node."""
+
     class Meta:
         model = Log
-        interfaces = (graphene.relay.Node, )
+        interfaces = (graphene.relay.Node,)
+
+
+class LogConnection(graphene.relay.Connection):
+    class Meta:
+        node = LogType
+
 
 class Query(graphene.ObjectType):
+    """Query objects for GraphQL API."""
+
     node = graphene.relay.Node.Field()
-    logs = SQLAlchemyConnectionField(LogObject)
+    logs = FilterConnectionField(LogConnection, process_id=graphene.String())
 
 schema = graphene.Schema(query=Query)
