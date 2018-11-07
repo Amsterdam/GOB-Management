913fa312875a2c9f249e7621ee12fada5c894b76
vegter <amsjurien@gmail.com>
2018-11-07 07:55:35 +0100

    Update models with new log data
    Add destination, catalogue and message id to Log class
    Include catalogue in SourceEntity class

--- src/gobmanagement/database/models.py
+++ src/gobmanagement/database/models.py
@@ -10,9 +10,12 @@ class Log(Base):
     timestamp = Column(DateTime)
     process_id = Column(String)
     source = Column(String)
+    destination = Column(String)
+    catalogue = Column(String)
     entity = Column(String)
     level = Column(String)
     name = Column(String)
+    msgid = Column("id", String)
     msg = Column(String)
     data = Column(JSON)
 
