f34b585f28bfa4d07965b1a2301813d547f3fd0c
vegter <amsjurien@gmail.com>
2018-11-21 10:49:00 +0100

    SQLAlchemy Management Model in core, migrations
    Use alembic for migrations
    Use Management Model that is maintained in GOB core

--- src/gobmanagement/database/base.py
+++ src/gobmanagement/database/base.py
@@ -1,16 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+import alembic.config
+
+from gobcore.model.sa.management import Base
+
 from .config import GOB_MANAGEMENT_DB
+
+# Database migrations are handled by alembic
+# alembic upgrade head
+alembicArgs = [
+    '--raiseerr',
+    'upgrade', 'head',
+]
+alembic.config.main(argv=alembicArgs)
 
 # Create database engine
 db_uri = URL(**GOB_MANAGEMENT_DB)
 engine = create_engine(db_uri)
 
 # Declarative base model to create database tables and classes
-Base = declarative_base()
 Base.metadata.bind = engine  # Bind engine to metadata of the base class
 
 # Create database session object
