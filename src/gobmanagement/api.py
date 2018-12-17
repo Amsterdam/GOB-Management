632318e4645cfe9eab3d0274fcd49dc6062c0425
vegter <amsjurien@gmail.com>
2018-12-17 15:14:01 +0100

    WebSocket interface for any new logs
    If any new log messages become available
    a broadcast event (new_logs) is emitted
    over a WebSocket

--- src/gobmanagement/api.py
+++ src/gobmanagement/api.py
@@ -1,16 +1,18 @@
 from flask_graphql import GraphQLView
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
-from gobmanagement.app import app, socketio
+from gobmanagement.app import app
 from gobmanagement.database.base import db_session
 from gobmanagement.schemas import schema
-
-CORS(app)
+from gobmanagement.socket import LogBroadcaster
 
 
 def _health():
     return 'Connectivity OK'
 
+
+CORS(app)
 
 _graphql = GraphQLView.as_view(
                 'graphql',
@@ -28,20 +30,20 @@ ROUTES = [
 for route, view_func in ROUTES:
     app.route(rule=route)(view_func)
 
+socketio = SocketIO(app, path="/gob_management/socket.io")
+logBroadcaster = LogBroadcaster(socketio)
+
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
 
-def some_function():
-    socketio.emit('some event', {'data': 42})
 
 @socketio.on('connect')
-def test_connect():
-    print("CONNECT")
-    socketio.emit('my response', {'data': 'Connected'})
+def socket_connect():
+    logBroadcaster.on_connect()
+
 
 @socketio.on('disconnect')
-def test_disconnect():
-    print("DISCONNECT")
-    print('Client disconnected')
+def socket_disconnect():
+    logBroadcaster.on_disconnect()
--- src/gobmanagement/app.py
+++ src/gobmanagement/app.py
@@ -1,8 +1,4 @@
 from flask import Flask
-from flask_socketio import SocketIO
 
 # app initialization
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_credentials=False, cors_allowed_origins="http://localhost:8080")
