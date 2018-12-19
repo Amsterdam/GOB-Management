2a2acd0aa7df0926d4958121880146b7c4862516
vegter <amsjurien@gmail.com>
2018-12-19 08:33:27 +0100

    config module added, cors limited
    Management configuration has been concentrated in config.py
    
    CORS support has been limited to known domains

--- src/gobmanagement/__main__.py
+++ src/gobmanagement/__main__.py
@@ -5,8 +5,7 @@ This module is the main module for the API server.
 On startup the gobmanagement is instantiated.
 
 """
-import os
-
+from gobmanagement.config import API_PORT
 from gobmanagement.api import app, socketio
 
-socketio.run(app=app, port=os.getenv("GOB_MANAGEMENT_PORT", 8143))
+socketio.run(app=app, port=API_PORT)
