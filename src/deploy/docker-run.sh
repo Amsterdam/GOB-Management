47520b28dd64ec88bd2a462c9befabeb6d5dbcf2
a.obdeijn <A.Obdeijn@spilgames.com>
2019-04-23 15:45:03 +0200

    Use http proxy for Gatekeeper if set

--- src/deploy/docker-run.sh
+++ src/deploy/docker-run.sh
@@ -4,6 +4,11 @@ set -e   # stop on any error
 set -x
 
 # run gatekeeper
-./keycloak-gatekeeper --config gatekeeper.conf 2>&1 | tee /var/log/gatekeeper/gatekeeper.log &
+
+if [ -n $HTTP_PROXY ]; then
+  ./keycloak-gatekeeper --config gatekeeper.conf --openid-provider-proxy $HTTP_PROXY 2>&1 | tee /var/log/gatekeeper/gatekeeper.log &
+else
+  ./keycloak-gatekeeper --config gatekeeper.conf 2>&1 | tee /var/log/gatekeeper/gatekeeper.log &
+fi
 
 exec uwsgi --gevent 100 --show-config --http-websockets --enable-threads --threads 4
