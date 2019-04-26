716bf4c60da92b823fe53d05aa09083f3bde86d4
a.obdeijn <A.Obdeijn@spilgames.com>
2019-04-26 12:32:05 +0200

    Make request buffer size larger. Fix bash conditional

--- src/deploy/docker-run.sh
+++ src/deploy/docker-run.sh
@@ -5,10 +5,11 @@ set -x
 
 # run gatekeeper
 
-if [ -n ${HTTP_PROXY:=} ]; then
+if [[ -n ${HTTP_PROXY:=} ]]; then
+  echo "Using $HTTP_PROXY as http proxy for Gatekeeper\n"
   ./keycloak-gatekeeper --config gatekeeper.conf --openid-provider-proxy $HTTP_PROXY 2>&1 | tee /var/log/gatekeeper/gatekeeper.log &
 else
   ./keycloak-gatekeeper --config gatekeeper.conf 2>&1 | tee /var/log/gatekeeper/gatekeeper.log &
 fi
 
-exec uwsgi --gevent 100 --show-config --http-websockets --enable-threads --threads 4
+exec uwsgi --gevent 100 --show-config --http-websockets --enable-threads --threads 4 -b 32768
