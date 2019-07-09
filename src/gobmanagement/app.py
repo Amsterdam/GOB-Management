fb6c5dfbdfe5d71a52b85fcc6527a9c2d18e7d54
Jaap Koetsier <jaap@chariot-it.nl>
2019-07-09 16:44:19 +0200

    Add gRPC server for starting jobs

--- src/gobmanagement/app.py
+++ src/gobmanagement/app.py
@@ -1,4 +1,15 @@
+import grpc
+
 from flask import Flask
+from concurrent import futures
+from gobmanagement.grpc.out import jobs_pb2_grpc
+from gobmanagement.grpc.services.jobs import JobsServicer
 
 # app initialization
 app = Flask(__name__)
+
+# start gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+jobs_pb2_grpc.add_JobsServicer_to_server(JobsServicer(), server)
+server.add_insecure_port('[::]:50051')
+server.start()
