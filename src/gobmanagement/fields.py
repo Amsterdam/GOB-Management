a0c9fcd10d782227cd4a72af55619e81064e7f73
Jaap Koetsier <jaap@chariot-it.nl>
2020-08-20 08:49:22 +0200

    PGB-1945 Hide datawarnings when requesting logs in GraphQL

--- src/gobmanagement/fields.py
+++ src/gobmanagement/fields.py
@@ -1,12 +1,15 @@
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
 
-class FilterConnectionField(SQLAlchemyConnectionField):
+class LogFilterConnectionField(SQLAlchemyConnectionField):
     RELAY_ARGS = ['first', 'last', 'before', 'after', 'sort']
 
     @classmethod
     def get_query(cls, model, info, **args):
-        query = super(FilterConnectionField, cls).get_query(model, info, **args)
+        query = super(LogFilterConnectionField, cls).get_query(model, info, **args)
+
+        # Do not return DATAWARNING, DATAERROR, DATAINFO
+        query = query.filter(getattr(model, 'level').notlike('DATA%'))
         for field, value in args.items():
             if field not in cls.RELAY_ARGS:
                 query = query.filter(getattr(model, field) == value)
